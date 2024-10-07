6991f47263ca255e9c16d020221b6f2e0e67db86
nakita-Ypm <nakita@ypm.llc>
2024-10-07 09:51:59 +0000

    add static function

--- src/karabiner/index.ts
+++ src/karabiner/index.ts
@@ -46,3 +46,86 @@ const astarte = [
     ['i', 'o', 'e', 'a', '.', 'k', 't', 'n', 's', 'r'],
     ['z', 'x', '-', 'c', '/', 'm', 'l', 'f', 'b', 'v'],
 ]
+
+export class Karabiner {
+    static karabinerDescription(title: string, description: string, result: any[]) {
+        return {
+            title: title,
+            rules: [
+                {
+                    description: description,
+                    manipulators: result
+                }
+            ]
+        };
+    }
+
+    static mandatoryKeymap(fromKey: string, toKey: string, mandatory: string[]) {
+        return {
+            type: "basic",
+            from: {
+                key_code: fromKey,
+                modifiers: {
+                    mandatory: mandatory,
+                }
+            },
+            to: [
+                {
+                    key_code: toKey,
+                    modifiers: mandatory,
+                }
+            ]
+        };
+    }
+
+    static jpMode(fromKey: string, toKey: string): any {
+        return {
+            type: "basic",
+            from: {
+                key_code: fromKey
+            },
+            to: [
+                {
+                    key_code: toKey
+                }
+            ],
+            conditions: [
+                {
+                    type: "input_source_if",
+                    input_sources: [
+                        {
+                            language: "ja"
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+
+    static toggle(lan: string, fromKey: string, toKey: string, opt: string[]) {
+        return {
+            type: "basic",
+            conditions: [
+                {
+                    input_sources: [
+                        {
+                            language: lan
+                        }
+                    ],
+                    type: "input_source_if"
+                }
+            ],
+            from: {
+                key_code: fromKey,
+                modifiers: {
+                    optional: opt
+                }
+            },
+            to: [
+                {
+                    key_code: toKey
+                }
+            ]
+        };
+    }
+}
