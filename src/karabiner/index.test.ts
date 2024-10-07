d54a77ab8211c04245341dcd7c9c32a85e7677a6
nakita-Ypm <nakita@ypm.llc>
2024-10-07 10:47:48 +0000

    keycodeの追加

--- src/karabiner/index.test.ts
+++ src/karabiner/index.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from 'bun:test'
 import { Karabiner } from '.'
+import { expected_en_jp_toggle } from './data'
 
 // enJpToggle
 test('enJpToggle', () => {
@@ -70,64 +71,5 @@ test('enJpToggle', () => {
     ],
   }
 
-  const expected_karabiner = {
-    title: 'Change the right command key to toggle between alphanumeric and kana',
-    rules: [
-      {
-        description: 'Kana Eisuu Toggle right_command',
-        manipulators: [
-          {
-            type: 'basic',
-            conditions: [
-              {
-                input_sources: [
-                  {
-                    language: 'ja',
-                  },
-                ],
-                type: 'input_source_if',
-              },
-            ],
-            from: {
-              key_code: 'right_command',
-              modifiers: {
-                optional: ['any'],
-              },
-            },
-            to: [
-              {
-                key_code: 'japanese_eisuu',
-              },
-            ],
-          },
-          {
-            type: 'basic',
-            conditions: [
-              {
-                input_sources: [
-                  {
-                    language: 'en',
-                  },
-                ],
-                type: 'input_source_if',
-              },
-            ],
-            from: {
-              key_code: 'right_command',
-              modifiers: {
-                optional: ['any'],
-              },
-            },
-            to: [
-              {
-                key_code: 'japanese_kana',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  }
-
-  expect(result).toEqual(expected_karabiner)
+  expect(result).toEqual(expected_en_jp_toggle)
 })
